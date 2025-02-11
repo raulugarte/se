1fa0470133a52a87786deeeeb9f57f8a59873f50
raulugarte <63106469+raulugarte@users.noreply.github.com>
2025-02-11 14:22:52 +0100

    Update tabs.js

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -1,98 +1,47 @@
-/**
- * @typedef TabInfo
- * @property {string} name
- * @property {HTMLElement} $tab
- * @property {HTMLElement} $content
- */
+// eslint-disable-next-line import/no-unresolved
+import { toClassName } from '../../scripts/aem.js';
 
-/**
- * @param {HTMLElement} $block
- * @return {TabInfo[]}
- */
-export function createTabs($block) {
-  const $ul = $block.querySelector('ul');
-  if (!$ul) {
-    return null;
-  }
-  /** @type TabInfo[] */
-  const tabs = [...$ul.querySelectorAll('li')].map(($li) => {
-    const title = $li.textContent;
-    const name = title.toLowerCase().trim();
-    return {
-      title,
-      name,
-      $tab: $li,
-    };
-  });
-  // move $ul below section div
-  $block.replaceChildren($ul);
+export default async function decorate(block) {
+  // build tablist
+  const tablist = document.createElement('div');
+  tablist.className = 'tabs-list';
+  tablist.setAttribute('role', 'tablist');
 
-  // search referenced sections and move them inside the tab-container
-  const $wrapper = $block.parentElement;
-  const $container = $wrapper.parentElement;
-  const $sections = document.querySelectorAll('[data-tab]');
+  // decorate tabs and tabpanels
+  const tabs = [...block.children].map((child) => child.firstElementChild);
+  tabs.forEach((tab, i) => {
+    const id = toClassName(tab.textContent);
 
-  // move the tab's sections before the tab riders.
-  [...$sections].forEach(($tabContent) => {
-    const name = $tabContent.dataset.tab.toLowerCase().trim();
-    /** @type TabInfo */
-    const tab = tabs.find((t) => t.name === name);
-    if (tab) {
-      const $el = document.createElement('div');
-      $el.classList.add('tab-item');
-      $el.append(...$tabContent.children);
-      $el.classList.add('hidden');
-      $container.insertBefore($el, $wrapper);
-      $tabContent.remove();
-      tab.$content = $el;
-    }
-  });
-  return tabs;
-}
+    // decorate tabpanel
+    const tabpanel = block.children[i];
+    tabpanel.className = 'tabs-panel';
+    tabpanel.id = `tabpanel-${id}`;
+    tabpanel.setAttribute('aria-hidden', !!i);
+    tabpanel.setAttribute('aria-labelledby', `tab-${id}`);
+    tabpanel.setAttribute('role', 'tabpanel');
 
-/**
- * @param {HTMLElement} $block
- */
-export default function decorate($block) {
-  const tabs = createTabs($block);
-
-  // move the tab riders in front
-  const $wrapper = $block.parentElement;
-  const $container = $wrapper.parentElement;
-  $container.insertBefore($wrapper, $container.firstElementChild);
-
-  tabs.forEach((tab, index) => {
-    const $button = document.createElement('button');
-    const { $tab, title, name } = tab;
-    $button.textContent = title;
-    $tab.replaceChildren($button);
-
-    $button.addEventListener('click', () => {
-      const $activeButton = $block.querySelector('button.active');
-      const blockPosition = $block.getBoundingClientRect().top;
-      const offsetPosition = blockPosition + window.scrollY - 80;
-
-      if ($activeButton !== $tab) {
-        $activeButton.classList.remove('active');
-        $button.classList.add('active');
-
-        tabs.forEach((t) => {
-          if (name === t.name) {
-            t.$content.classList.remove('hidden');
-          } else {
-            t.$content.classList.add('hidden');
-          }
-          window.scrollTo({
-            top: offsetPosition,
-            behavior: 'smooth',
-          });
-        });
-      }
+    // build tab button
+    const button = document.createElement('button');
+    button.className = 'tabs-tab';
+    button.id = `tab-${id}`;
+    button.innerHTML = tab.innerHTML;
+    button.setAttribute('aria-controls', `tabpanel-${id}`);
+    button.setAttribute('aria-selected', !i);
+    button.setAttribute('role', 'tab');
+    button.setAttribute('type', 'button');
+    button.addEventListener('click', () => {
+      block.querySelectorAll('[role=tabpanel]').forEach((panel) => {
+        panel.setAttribute('aria-hidden', true);
+      });
+      tablist.querySelectorAll('button').forEach((btn) => {
+        btn.setAttribute('aria-selected', false);
+      });
+      tabpanel.setAttribute('aria-hidden', false);
+      button.setAttribute('aria-selected', true);
     });
-
-    if (index === 0) {
-      $button.classList.add('active');
-      tab.$content.classList.remove('hidden');
-    }
+    tablist.append(button);
+    tab.remove();
   });
+
+  block.prepend(tablist);
 }
